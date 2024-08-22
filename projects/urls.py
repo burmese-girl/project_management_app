3d9f2b7c3d7c71eabb5824c374107791086d262a
May Yi Aung <saunghninsi91@gmail.com>
2024-08-22 22:33:56 +0400

    adjust dashboard barchat

--- projects/urls.py
+++ projects/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import view_projects, view_dashboard
 
 urlpatterns = [
-    path('projects', view_projects.ProjectListView.as_view(), name='project_list'),
+    path('project_list', view_projects.ProjectListView.as_view(), name='project_list'),
     path('dashboard/', view_dashboard.dashboard, name='dashboard'),
     path('projects/<int:pk>/', view_projects.ProjectDetailView.as_view(), name='project_detail'),
 ]
