97cfe8f6fbf33dde767e4f811899c8022b604f1a
May Yi Aung <saunghninsi91@gmail.com>
2024-08-23 18:12:35 +0400

    edit project ui

--- projects/urls.py
+++ projects/urls.py
@@ -4,5 +4,6 @@ from .views import view_projects, view_dashboard
 urlpatterns = [
     path('project_list', view_projects.ProjectListView.as_view(), name='project_list'),
     path('dashboard/', view_dashboard.dashboard, name='dashboard'),
-    path('projects/<int:pk>/', view_projects.ProjectDetailView.as_view(), name='project_detail'),
+    path('project_detail/<int:pk>/', view_projects.ProjectDetailView.as_view(), name='project_detail'),
+    path('project_update/<int:pk>/', view_projects.ProjectUpdateView.as_view(), name='project_update'),
 ]
