38df1aced47eeeffdb344a4ecede14782553be4b
Phyo Pyae Sone <phyopyaesone.developer@gmail.com>
2024-09-16 17:00:48 +0630

    feat[RBAC]: view update to see only participated project, add default profile image, Y-M-D error fix

--- projects/models.py
+++ projects/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from .constants import STATUS_CHOICES, PROJECT_ROLES
 
 class Project(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, blank=False, null=False)
     description = models.TextField(blank=True,null=True)
     start_date = models.DateField(blank=True,null=True)
     end_date = models.DateField(blank=True,null=True)
@@ -25,18 +25,17 @@ class Task(models.Model):
         return self.title
 
 class TeamMember(models.Model):
-    # project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='team_members')
     role = models.CharField(max_length=100, choices = PROJECT_ROLES, default= 'staff')
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name='team_memberships')
 
     def __str__(self):
-        return self.user.username
+        return f'{self.user.username}-{self.role}'
     
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
     birth_date = models.DateField(null=True, blank=True)
-    profile_picture = models.ImageField(upload_to="profile_pics/", null=True, blank=True)
+    profile_picture = models.ImageField(upload_to="profile_pics/", null=True, blank=True, default='profile_pics/default.png')
 
     def __str__(self) -> str:
         return f'{self.user.username} Profile'
